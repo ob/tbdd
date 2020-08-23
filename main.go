c5b73f7dfd098173335852dc7ed8e451a4d8aa6d
Oscar Bonilla <6f6231@gmail.com>
2020-08-22 20:08:04 -0700

    First cut, it can store/retrieve tarballs

--- main.go
+++ main.go
@@ -1,18 +1,182 @@
 package main
 
 import (
+	"archive/tar"
+	"compress/gzip"
+	"crypto/md5"
+	"encoding/gob"
+	"encoding/hex"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path"
 )
 
+const dataDir = "storeDir/dataDir"
+const indexDir = "storeDir/indexDir"
+const tempDir = "storeDir/tempDir"
+
+func saveTarball(name string, gz io.Reader, meta io.Writer) error {
+	tmpIndex, err := ioutil.TempFile(tempDir, "index")
+	defer tmpIndex.Close()
+	if err != nil {
+		return err
+	}
+	enc := gob.NewEncoder(tmpIndex)
+	tb, err := gzip.NewReader(gz)
+	if err != nil {
+		return err
+	}
+	tarReader := tar.NewReader(tb)
+
+	for {
+		header, err := tarReader.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+		// save metadata
+		err = enc.Encode(header)
+		if err != nil {
+			return err
+		}
+
+		switch header.Typeflag {
+		case tar.TypeReg:
+			tmpfile, err := ioutil.TempFile(tempDir, "data")
+			defer tmpfile.Close()
+			if err != nil {
+				return err
+			}
+			hash := md5.New()
+			multiwriter := io.MultiWriter(hash, tmpfile)
+			io.Copy(multiwriter, tarReader)
+			md5sum := hex.EncodeToString(hash.Sum(nil))
+			// store it
+			destDir := path.Join(dataDir, md5sum[:2])
+			os.MkdirAll(destDir, 0755)
+			destFile := path.Join(destDir, md5sum)
+			// this needs better error checking
+			if _, err = os.Stat(destFile); os.IsNotExist(err) {
+				os.Rename(tmpfile.Name(), path.Join(destDir, md5sum))
+			} else {
+				os.Remove(tmpfile.Name())
+			}
+			err = enc.Encode(md5sum)
+			if err != nil {
+				return err
+			}
+		default:
+			// skip it
+		}
+	}
+	// save the index
+	destIndexFile := path.Join(indexDir, name)
+	_, err = os.Stat(destIndexFile)
+	if err != nil && os.IsNotExist(err) {
+		os.Rename(tmpIndex.Name(), destIndexFile)
+	} else if err != nil {
+		return err
+	}
+	return nil
+}
+
+func fetchTarBall(name string, w io.Writer) error {
+	indexFile := path.Join(indexDir, name)
+	fmt.Println("Looking for " + indexFile)
+	if _, err := os.Stat(indexFile); os.IsNotExist(err) {
+		fmt.Println("failed")
+		return err
+	}
+	f, err := os.Open(indexFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	dec := gob.NewDecoder(f)
+	gzw := gzip.NewWriter(w)
+	defer gzw.Close()
+	tarWriter := tar.NewWriter(gzw)
+	defer tarWriter.Close()
+	for {
+		var header tar.Header
+		err := dec.Decode(&header)
+		fmt.Println(header)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("bailing")
+				break
+			}
+			return err
+		}
+		tarWriter.WriteHeader(&header)
+		if header.Typeflag != tar.TypeReg {
+			continue
+		}
+		var md5sum string
+		err = dec.Decode(&md5sum)
+		if err != nil {
+			return err
+		}
+		dataFile := path.Join(dataDir, md5sum[:2], md5sum)
+		data, err := os.Open(dataFile)
+		if err != nil {
+			return err
+		}
+		defer data.Close()
+		io.Copy(tarWriter, data)
+	}
+	return nil
+}
+
 func handle(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world")
+	defer r.Body.Close()
+
+	// strip leading '/'
+	filename := r.URL.Path[1:]
+	fmt.Println(filename)
+
+	switch m := r.Method; m {
+	case http.MethodGet:
+		fmt.Println("Fetching: " + filename)
+		w.Header().Set("Content-Type", "application/gzip")
+		err := fetchTarBall(filename, w)
+		if err != nil {
+			fmt.Println(err)
+		}
+	case http.MethodPut:
+		fmt.Println("Stashing: " + filename)
+		err := saveTarball(filename, r.Body, ioutil.Discard)
+		if err != nil {
+			fmt.Println(err)
+		}
+	default:
+		fmt.Println("Dunno what to do!")
+		http.Error(w, "Dunno what you mean dude", http.StatusMethodNotAllowed)
+	}
 }
 
 func main() {
 	portNumber := "6515"
-	http.HandleFunc("/", handle)
-	fmt.Printf("Server listening on http://localhost:%s\n", portNumber)
-	http.ListenAndServe(":"+portNumber, nil)
+
+	// create dirs
+	os.MkdirAll(dataDir, 0755)
+	os.MkdirAll(tempDir, 0755)
+	os.MkdirAll(indexDir, 0755)
+	mux := http.NewServeMux()
+	httpServer := &http.Server{
+		Addr:         "127.0.0.1:" + portNumber,
+		Handler:      mux,
+		ReadTimeout:  0,
+		WriteTimeout: 0,
+	}
+	mux.HandleFunc("/", handle)
+	fmt.Printf("Server listening on http://%s\n", httpServer.Addr)
+	httpServer.ListenAndServe()
 }
